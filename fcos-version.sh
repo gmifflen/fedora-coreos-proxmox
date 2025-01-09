e47978879d8deac420762f4ecd3a36a9eb433bc8
Gabe Mifflen <115088238+gmifflen@users.noreply.github.com>
2025-01-09 01:47:48 -0400

    Update fcos-version.sh

--- fcos-version.sh
+++ fcos-version.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+source ./colours.sh
+
 # URL to fetch the stable release JSON
 RELEASE_JSON="https://builds.coreos.fedoraproject.org/streams/stable.json"
 # Fetch the JSON data and extract the stable release number using jq
 VERSION=$(curl -s $RELEASE_JSON | jq -r '.architectures.x86_64.artifacts.qemu.release')
 if [ $? -ne 0 ]; then
-    echo "Failed to fetch the stable release JSON from $RELEASE_JSON"
+    print_error "Failed to fetch the stable release JSON from $RELEASE_JSON"
     exit 1
 fi
 
 # Print the stable release number
-echo "Current stable release number: $VERSION"
+print_info "Current stable release number: $VERSION"
